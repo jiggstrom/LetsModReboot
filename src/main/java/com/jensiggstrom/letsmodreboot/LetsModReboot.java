c1daba6757c5377e69d474d798cd0fc20db56cc4
Jens Iggstrom <jens.iggstrom@gmail.com>
2016-02-12 11:19:45 +0100

    Setup project metadata

---
+++ src/main/java/com/jensiggstrom/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,15 @@
+package com.jensiggstrom.letsmodreboot;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = LetsModReboot.MODID, version = LetsModReboot.VERSION)
+public class LetsModReboot
+{
+    public static final String MODID = "LetsModReboot";
+    public static final String VERSION = "1.7.10-1.0";
+
+
+}
